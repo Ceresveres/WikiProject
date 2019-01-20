b514fda8617fe3f277bb1a98dd7dc43bfba57938
ceresveres <ceresveres@gmail.com>
2019-01-20 09:47:32 -0900

    configured custom routes for static pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,8 @@
 Rails.application.routes.draw do
-<<<<<<< HEAD
   resources :wikis
-=======
-  get 'static_pages/home'
-  get 'static_pages/help'
-  get 'static_pages/contact'
-  get 'static_pages/about'
->>>>>>> b68ea0f6848f4b23d46644218e7e7635fefc881e
+  get 'help',		 to: 'static_pages#help'
+  get 'contact', to: 'static_pages#contact'
+  get 'about',	 to: 'static_pages#about'
+  root to: 'static_pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
