c78eb9c4d311a17ebc60be6e9fce67c49c2ef483
ceresveres <ceresveres@gmail.com>
2019-01-20 09:31:18 -0900

    seeded initial test data for wiki

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+5.times do |wiki|
+	Wiki.create!(
+		title: 	"Sample wiki #{wiki}",
+		body: 	"This is a few sample text to describe the wiki"
+	)
+end
+
+puts "5 wikis created"
