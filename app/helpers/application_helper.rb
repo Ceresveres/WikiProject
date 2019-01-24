6c8e62a210a5424fb49d9aaa04f2b92a60b32e78
ceresveres <ceresveres@gmail.com>
2019-01-23 15:48:26 -0900

    Implemented a login helper for application layout

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -12,11 +12,11 @@ module ApplicationHelper
     ]
   end
 
-	def nav_helper style, tag_type
+	def nav_helper style
     nav_links = ''
 
     nav_items.each do |item|
-      nav_links << "<#{tag_type}><a href='#{item[:url]}' class='#{style} #{active? item[:url]}'>#{item[:title]}</a></#{tag_type}>"
+      nav_links << "<a href='#{item[:url]}' class='#{style} #{active? item[:url]}'>#{item[:title]}</a>"
     end
 
     nav_links.html_safe
@@ -24,5 +24,14 @@ module ApplicationHelper
 
   def active? path
     "active" if current_page? path
-  end 
+  end
+
+  def login_helper style = ''
+  	if current_user
+  		"<a href='#{destroy_user_session_path}' rel='nofollow' data-method='delete' class='#{style}'>Logout</a>".html_safe
+  	else
+  		"<a href='#{new_user_registration_path}' class='#{style}'>Register</a>".html_safe + " ".html_safe +
+  		"<a href='#{user_session_path}' class='#{style}'>Login</a>".html_safe
+    end
+  end
 end
